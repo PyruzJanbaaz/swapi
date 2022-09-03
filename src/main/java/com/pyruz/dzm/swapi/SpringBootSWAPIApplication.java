72d2950b98890c886dec5a5309401d168d886f0e
pyruz <pyruz.janbaaz@gmail.com>
2022-09-03 18:07:35 +0400

    Initial commit

---
+++ src/main/java/com/pyruz/dzm/swapi/SpringBootSWAPIApplication.java
@@ -0,0 +1,13 @@
+package com.pyruz.dzm.swapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSWAPIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSWAPIApplication.class, args);
+	}
+
+}
