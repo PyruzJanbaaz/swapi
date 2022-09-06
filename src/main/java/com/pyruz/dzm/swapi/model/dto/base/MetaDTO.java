96bdd5e074b9ab2e5a1680282f47240f345b9684
pyruz <pyruz.janbaaz@gmail.com>
2022-09-06 22:04:42 +0400

    - PeopleController - PeopleService

--- src/main/java/com/pyruz/dzm/swapi/model/dto/base/MetaDTO.java
+++ src/main/java/com/pyruz/dzm/swapi/model/dto/base/MetaDTO.java
@@ -1,6 +1,6 @@
 package com.pyruz.dzm.swapi.model.dto.base;
 
-import com.pyruz.dzm.swapi.utility.ApplicationProperties;
+import com.pyruz.dzm.swapi.utility.ApplicationMessages;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,15 +15,7 @@ public class MetaDTO {
     private Integer code;
     private String message;
 
-    public static MetaDTO getInstance(ApplicationProperties applicationProperties) {
-        return new MetaDTO(
-                HttpStatus.OK.value(),
-                applicationProperties.getProperty("application.message.operation.success.text")
-        );
+    public static MetaDTO getInstance(ApplicationMessages applicationMessages) {
+        return new MetaDTO(HttpStatus.OK.value(), applicationMessages.getProperty("application.message.success.text"));
     }
-
-    public static MetaDTO getInstance(Integer code, String message) {
-        return new MetaDTO(code, message);
-    }
-
 }
