44ea5f2c8fb412e53139d6db89449e67732a9734
pyruz <pyruz.janbaaz@gmail.com>
2022-09-11 20:00:44 +0400

    - code cleaning

--- src/main/java/com/pyruz/dzm/swapi/SpringBootSWAPIApplication.java
+++ src/main/java/com/pyruz/dzm/swapi/SpringBootSWAPIApplication.java
@@ -4,16 +4,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-
-import java.time.Duration;
 
 @SpringBootApplication
-@EnableCaching
 public class SpringBootSWAPIApplication {
 
     public static void main(String[] args) {
@@ -23,17 +16,6 @@ public class SpringBootSWAPIApplication {
     @Bean
     public Logger getLogger() {
         return LogManager.getLogger();
-    }
-
-    @Bean
-    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
-        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
-                .prefixCacheNameWith(this.getClass().getPackageName() + ".")
-                .entryTtl(Duration.ofHours(1))
-                .disableCachingNullValues();
-        return RedisCacheManager.builder(connectionFactory)
-                .cacheDefaults(config)
-                .build();
     }
 
 }
